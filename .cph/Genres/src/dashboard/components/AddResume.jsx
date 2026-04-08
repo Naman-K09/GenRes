4db9b22d9144e98e281094b425a5f28951d71a19
Naman-K09 <namanitsmenaman@gmail.com>
2026-04-08 12:19:53 +0530

    Save changes

--- .cph/Genres/src/dashboard/components/AddResume.jsx
+++ .cph/Genres/src/dashboard/components/AddResume.jsx
@@ -1,47 +1,93 @@
-import React, { useEffect, useState } from 'react'
-import AddResume from './components/AddResume'
+import { Loader2, PlusSquare } from 'lucide-react'
+import React, { useState } from 'react'
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+  } from "@/components/ui/dialog"
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { v4 as uuidv4 } from 'uuid';
+import GlobalApi from './../../../service/GlobalApi'
 import { useUser } from '@clerk/clerk-react'
-import GlobalApi from './../../service/GlobalApi';
-import ResumeCardItem from './components/ResumeCardItem';
+import { Navigate, useNavigate } from 'react-router-dom'
 
-function Dashboard() {
+function AddResume() {
 
-  const {user}=useUser();
-  const [resumeList,setResumeList]=useState([]);
-  useEffect(()=>{
-    user&&GetResumesList()
-  },[user])
-
-  /**
-   * Used to Get Users Resume List
-   */
-  const GetResumesList=()=>{
-    GlobalApi.GetUserResumes(user?.primaryEmailAddress?.emailAddress)
-    .then(resp=>{
-      console.log(resp.data.data)
-      setResumeList(resp.data.data);
-    })
-  }
-  return (
-    <div className='p-10 md:px-20 lg:px-32'>
-      <h2 className='font-bold text-3xl'>My Resume</h2>
-      <p>Start Creating AI resume to your next Job role</p>
-      <div className='grid grid-cols-2 
-      md:grid-cols-3 lg:grid-cols-5 gap-5
-      mt-10
-      '>
-        <AddResume/>
-        {resumeList.length>0?resumeList.map((resume,index)=>(
-          <ResumeCardItem resume={resume} key={index} refreshData={GetResumesList} />
-        )):
-        [1,2,3,4].map((item,index)=>(
-          <div className='h-[280px] rounded-lg bg-slate-200 animate-pulse'>
-          </div>
-        ))
+    const [openDialog,setOpenDialog]=useState(false)
+    const [resumeTitle,setResumeTitle]=useState();
+    const {user}=useUser();
+    const [loading,setLoading]=useState(false);
+    const navigation=useNavigate();
+    const onCreate=async()=>{
+        setLoading(true)
+        const uuid=uuidv4();
+        const data={
+            data:{
+                title:resumeTitle,
+                resumeId:uuid,
+                userEmail:user?.primaryEmailAddress?.emailAddress,
+                userName:user?.fullName
+            }
         }
-      </div>
+
+        GlobalApi.CreateNewResume(data).then(resp=>{
+            console.log(resp.data.data.documentId);
+            if(resp)
+            {
+                setLoading(false);
+                navigation('/dashboard/resume/'+resp.data.data.documentId+"/edit");
+            }
+        },(error)=>{
+            setLoading(false);
+        })
+
+    }
+  return (
+    <div >
+        <div className='p-14 py-24 border 
+        items-center flex 
+        justify-center bg-secondary
+        rounded-lg h-[280px]
+        hover:scale-105 transition-all hover:shadow-md
+        cursor-pointer border-dashed'
+        onClick={()=>setOpenDialog(true)}
+        >
+            <PlusSquare  />
+        </div>
+
+        <Dialog open={openDialog}>
+       
+        <DialogContent>
+            <DialogHeader>
+            <DialogTitle>Create New Resume</DialogTitle>
+            <DialogDescription>
+                <p>Add a title for your new resume</p>
+         
+         RES       <Input className="my-2" 
+                placeholder="Ex.Full Stack resume"
+                onChange={(e)=>setResumeTitle(e.target.value)}
+                />
+            </DialogDescription>
+            <div className='flex justify-end gap-5'>
+                <Button onClick={()=>setOpenDialog(false)} variant="ghost">Cancel</Button>
+                <Button 
+                    disabled={!resumeTitle||loading}
+                onClick={()=>onCreate()}>
+                    {loading?
+                    <Loader2 className='animate-spin' /> :'Create'   
+                }
+                    </Button>
+            </div>
+            </DialogHeader>
+        </DialogContent>
+        </Dialog>
+
     </div>
   )
 }
 
-export default Dashboard
+export default AddResume
